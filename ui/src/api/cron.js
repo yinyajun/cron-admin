13066777d99caf5f95eeb63e29f9f10e726f0f24
yinyajun-hj <yinyajun-hj@huafang.com>
2023-03-17 22:46:24 +0800

    update

--- ui/src/api/cron.js
+++ ui/src/api/cron.js
@@ -1,116 +1,116 @@
 import axios from 'axios'
 import {
-	Message,
-	MessageBox
+  Message,
+  MessageBox
 } from 'element-ui'
 
 const service = axios.create({
-	baseURL: 'http://localhost:8080',
-	timeout: 5000
+  baseURL: "./",
+  timeout: 3000
 })
 
 export function schedule() {
-	return service({
-		url: '/api/v1/schedule',
-		method: 'get'
-	})
+  return service({
+    url: '/api/v1/schedule',
+    method: 'get'
+  })
 }
 
 
 export function active(job) {
-	return service({
-		url: '/api/v1/active',
-		method: 'get',
-		params: {
-			job
-		}
-	})
+  return service({
+    url: '/api/v1/active',
+    method: 'get',
+    params: {
+      job
+    }
+  })
 }
 
 
 export function pause(job) {
-	return service({
-		url: '/api/v1/pause',
-		method: 'get',
-		params: {
-			job
-		}
-	})
+  return service({
+    url: '/api/v1/pause',
+    method: 'get',
+    params: {
+      job
+    }
+  })
 }
 
 export function remove(job) {
-	return service({
-		url: '/api/v1/remove',
-		method: 'get',
-		params: {
-			job
-		}
-	})
+  return service({
+    url: '/api/v1/remove',
+    method: 'get',
+    params: {
+      job
+    }
+  })
 }
 
 
 export function add(spec, job) {
-	return service({
-		url: '/api/v1/add',
-		method: 'get',
-		params: {
-			spec,
-			job
-		}
-	})
+  return service({
+    url: '/api/v1/add',
+    method: 'get',
+    params: {
+      spec,
+      job
+    }
+  })
 }
 
 
 export function history(job) {
-	return service({
-		url: '/api/v1/history',
-		method: 'get',
-		params: {
-			job
-		}
-	})
+  return service({
+    url: '/api/v1/history',
+    method: 'get',
+    params: {
+      job
+    }
+  })
 }
 
 
 export function jobs() {
-	return service({
-		url: '/api/v1/jobs',
-		method: 'get'
-	})
+  return service({
+    url: '/api/v1/jobs',
+    method: 'get'
+  })
 }
 
 export function running() {
-	return service({
-		url: '/api/v1/running',
-		method: 'get'
-	})
+  return service({
+    url: '/api/v1/running',
+    method: 'get'
+  })
 }
 
 
 export function execute(job) {
-	return service({
-		url: '/api/v1/execute',
-		method: 'get',
-		params: {
-			job
-		}
-	})
+  return service({
+    url: '/api/v1/execute',
+    method: 'get',
+    params: {
+      job
+    }
+  })
 }
 
 // response interceptor
 service.interceptors.response.use(
-	response => {
-		const res = response.data
-		if (res.code !== 0) {
-			const errMsg = 'Internal Error: ' + res.code
-			console.error(res.code, res.msg)
-			return Promise.reject(errMsg)
-		} else {
-			return res
-		}
-	},
-	error => {
-		console.error(error.message)
-		return Promise.reject(error)
-	}
+  response => {
+    const res = response.data
+    if (res.code !== 0) {
+      const errMsg = 'Internal Error: ' + res.code
+      console.error(res.code, res.msg)
+      return Promise.reject(errMsg)
+    } else {
+      return res
+    }
+  },
+  error => {
+    console.error(error.message)
+    return Promise.reject(error)
+  }
 )
